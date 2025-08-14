75001940cdf0387b6035de1a4e74c1a3ff662bd5
FredyClinton <164343623+FredyClinton@users.noreply.github.com>
2025-08-14 11:56:02 +0100

    fin

--- src/main/java/therooster/JRGStudentCard/controller/ReportController.java
+++ src/main/java/therooster/JRGStudentCard/controller/ReportController.java
@@ -55,7 +55,7 @@ public class ReportController {
                     .body(pdfData);
 
         } catch (JRException e) {
-            throw new RuntimeException( "Error generated during the compilation report", e);
+            throw new RuntimeException( "Error generated  the compilation report", e);
         }
     }
 
